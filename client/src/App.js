32f3d86104a84902af4e79876c0682adb6ef4022
Santiago Trini <santiagotrini@gmail.com>
2022-11-16 16:47:51 -0300

    parece que anda

--- client/src/App.js
+++ client/src/App.js
@@ -6,29 +6,54 @@ const App = () => {
   const fileInput = useRef(null);
 
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    // console.log(fileInput.current.files[0]);
-    // console.log(title);
-    // const data = {
-    //   image: fileInput.current.files[0],
-    //   title: title
-    // };
+  const handleSubmit = async (event) => {
+    event.preventDefault()
     const formData = new FormData();
-    formData.append('image', fileInput);
-    formData.append('title', title);
-    console.log(formData);
-    axios.post('/uploads', formData, { headers: { "Content-Type": "multipart/form-data" } })
-      .then(res => console.log(res.data))
-      .catch(err => console.error(err));
-  };
+    formData.append("image", fileInput.current.files[0]);
+    formData.append("title", title);
+    const url = '/uploads';
+    const data = formData;
+    const config = {
+      headers: { "Content-Type": "multipart/form-data" }
+    };
+    axios.post(url,data,config)
+      .then(res => console.log(res.data));
+    // try {
+    //   const response = await axios({
+    //     method: "post",
+    //     url: "/uploads",
+    //     data: formData,
+    //     headers: { "Content-Type": "multipart/form-data" },
+    //   });
+    // } catch (error) {
+    //   console.err(error)
+    // }
+  }
+
+  // const handleSubmit = e => {
+  //   e.preventDefault();
+  //   // console.log(fileInput.current.files[0]);
+  //   // console.log(title);
+  //   // const data = {
+  //   //   image: fileInput.current.files[0],
+  //   //   title: title
+  //   // };
+  //   const formData = new FormData();
+  //   formData.append('image', fileInput);
+  //   formData.append('title', title);
+  //   console.log(formData);
+  //   axios.post('/uploads', formData, { headers: { "Content-Type": "multipart/form-data" } })
+  //     .then(res => console.log(res.data))
+  //     .catch(err => console.error(err));
+  // };
 
   const [title, setTitle] = useState('');
 
   return (
     <div>
       <h2>Subi un archivo</h2>
-      <form onSubmit={handleSubmit} encType="multipart/form-data">
+      <form onSubmit={handleSubmit}>
+      {/*<form method="post" action="/uploads" encType="multipart/form-data">*/}
         <label>titulo</label>
         <input
           type="text"
